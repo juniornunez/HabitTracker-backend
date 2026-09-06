3ef6c383fb23c2621fe1df64caae157d9a535462
Junior Nuñez <moisesmnuez@gmail.com>
2026-09-06 17:20:02 -0600

    Enable CORS to allow requests from frontend origin

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Backend corriendo en http://localhost:${port}`);
